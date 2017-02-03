beb9e99babddfec4077f1584ff9a5f3a03c9379b
tuk4maar <jv.rasa@hotmail.com>
2017-02-03 09:19:44 +0100

    Update HalloRocDev.java
    Hallo van Patrick

--- src/h01/HalloRocDev.java
+++ src/h01/HalloRocDev.java
@@ -6,6 +6,6 @@ package h01;
 public class HalloRocDev {
 
     public static void main(String[] args) {
-        System.out.println("Hallo van Kameel");
+        System.out.println("Hallo van Patrick");
     }
 }
